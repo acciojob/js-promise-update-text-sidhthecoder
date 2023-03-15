7413fee74a3d7a5dc6abcd0035c2e68434a5ffa3
Siddhant Maurya <88020811+sidhthecoder@users.noreply.github.com>
2023-03-15 22:35:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+let output=document.getElementById("output");
+
+function promise(){
+	return new promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve("Hello,world");
+		});
+	});
+}
+
+
+output.innerHTML=promise().then((data)=>console.log("data",data));;
+/*
+function giveFood() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // resolve("giving food...");
+      reject("no bread ");
+    }, 1000);
+  });
+}
+myPromiseFnc().then((data) => console.log("data", data));
+*/
